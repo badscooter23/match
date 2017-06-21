b59eadf861f1acf7b67662607fbe819605165e58
Scott McClellan <scott@h2o.ai>
2017-06-21 14:03:18 -0700

    initial checkin

---
+++ example.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+)
+
+func main() {
+
+	args := os.Args
+	numArgs := len(args)
+
+	if numArgs != 3 {
+		fmt.Println("expected: ./match regexp string")
+	}
+
+	p := args[1]
+	s := args[2]
+
+	fmt.Println("Checking to see if ", p, "matches", s)
+
+	fmt.Println("... compare using regex.MatchString()... ")
+	match, e := regexp.MatchString(p, s)
+
+	if e != nil {
+		fmt.Println("error", e)
+	} else {
+		fmt.Println("result: ", match)
+	}
+
+	fmt.Println()
+
+	fmt.Println("... compare using filepath.Match()... ")
+	match, e = filepath.Match(p, s)
+	if e != nil {
+		fmt.Println("error", e)
+	} else {
+		fmt.Println("result: ", match)
+	}
+
+}
